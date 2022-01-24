c85a96d17dc1191a59faef079c799495f9176926
Chloe Banliat <chloe.banliat@isen.yncrea.fr>
2022-01-24 17:12:28 +0100

    toast button1

--- app/src/main/java/fr/isen/banliat/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/banliat/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,21 @@ package fr.isen.banliat.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var button1 = findViewById(R.id.button2) as Button
+
+        button1.setOnClickListener {
+            // toast on button click event
+            Toast.makeText(this, "vous avez choisiie les entrées.", Toast.LENGTH_LONG).show()
+        }
     }
+
 }
