ea2d089ba3efa6a299d95e87a050174f20d4044c
Chloe Banliat <chloe.banliat@isen.yncrea.fr>
2022-02-04 17:26:08 +0100

    creation du compte requete POST

--- app/src/main/java/fr/isen/banliat/androiderestaurant/RegisterActivity.kt
+++ app/src/main/java/fr/isen/banliat/androiderestaurant/RegisterActivity.kt
@@ -19,61 +19,106 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
 
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val lastName = binding.lastName.text.toString()
-        val firstName = binding.firstName.text.toString()
-        val address = binding.address.text.toString()
-        val email = binding.email.text.toString()
-        val password = binding.password.text.toString()
-
-
         binding.createBtn.setOnClickListener {
-            if (lastName.isEmpty()) {
-                Toast.makeText(this, "Entrer un nom", Toast.LENGTH_LONG).show()
-            } else if (firstName.isEmpty()) {
-                Toast.makeText(this, "Entrer un prénom", Toast.LENGTH_LONG).show()
-            } else if (address.isEmpty()) {
-                Toast.makeText(this, "Entrer une adress", Toast.LENGTH_LONG).show()
-            } else if (email.isEmpty()) {
-                Toast.makeText(this, "Entrer une adresse mail", Toast.LENGTH_LONG).show()
-            } else if (password.isEmpty()) {
-                Toast.makeText(this, "Entrer un mot de passe", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this, "Compte créer", Toast.LENGTH_LONG).show()
+            if(validationFistName() && validationLastName() && validationEmail() && validAdress() && validPassword()){
+
+                Toast.makeText(this, "creation du compte", Toast.LENGTH_LONG).show()
+
+                val firstname = binding.firstName.text.toString()
+                val lastname = binding.lastName.text.toString()
+                val address = binding.address.text.toString()
+                val email = binding.email.text.toString()
+                val password = binding.password.text.toString()
+
+
+                val url = "http://test.api.catering.bluecodegames.com/user/register"
+                val jsonObject = JSONObject()
+                jsonObject.put("id_shop", "1")
+                jsonObject.put("firstname", firstname)
+                jsonObject.put("lastname", lastname)
+                jsonObject.put("address", address)
+                jsonObject.put("email", email)
+                jsonObject.put("password", password)
+
+
+                val jsonRequest = JsonObjectRequest(
+                    Request.Method.POST, url, jsonObject, { response ->
+                        var gson = Gson()
+
+                    }, {
+                        Log.e("", "erreur lors de la récupération")
+                    })
+                Volley.newRequestQueue(this).add(jsonRequest)
             }
         }
 
 
 
-        /*
-        //request POST
-
-        val url = "http://test.api.catering.bluecodegames.com/user/register"
-        val jsonObject = JSONObject()
-            jsonObject.put("id_shop", 1)
-            jsonObject.put("firstname", firstName)
-            jsonObject.put("lastname", lastName)
-            jsonObject.put("address", address)
-            jsonObject.put("email", email)
-            jsonObject.put("password", password)
-
-            val jsonRequest = JsonObjectRequest(
-                Request.Method.POST, url, jsonObject, { response ->
-                    var gson = Gson()
-                    Log.d("", "message")
-                }, {
-                    Log.e("", "erreur lors de la récupération de la liste des plats")
-                })
-            Volley.newRequestQueue(this).add(jsonRequest)
-
-         */
+    }
+    private fun validationFistName(): Boolean {
+        if (binding.firstName.text.toString().trim().isEmpty()) {
+            Toast.makeText(this,"veuillez saisir votre prénom",Toast.LENGTH_LONG).show()
+            binding.firstName.requestFocus()
+            return false
         }
+        return true
+    }
+
+    private fun validationLastName(): Boolean {
+        if (binding.lastName.text.toString().trim().isEmpty()) {
+            Toast.makeText(this,"veuillez saisir votre nom",Toast.LENGTH_LONG).show()
+            binding.lastName.requestFocus()
+            return false
+        }
+        return true
+    }
+
+    private fun validationEmail(): Boolean {
+        if (binding.email.text.toString().trim().isEmpty()) {
+            Toast.makeText(this,"veuillez saisir un email",Toast.LENGTH_LONG).show()
+            binding.email.requestFocus()
+            return false
+        }
+        return true
+    }
+
+    private fun validAdress(): Boolean {
+        if (binding.email.text.toString().trim().isEmpty()) {
+            Toast.makeText(this,"veuillez saisir un email",Toast.LENGTH_LONG).show()
+            binding.email.requestFocus()
+            return false
+        }
+        return true
+    }
+
+    private fun validPassword(): Boolean {
+        if (binding.password.text.toString().trim().isEmpty()) {
+            Toast.makeText(this,"veuillez saisir un mot de passe",Toast.LENGTH_LONG).show()
+            binding.password.requestFocus()
+            return false
+        } else if (binding.password.text.toString().length < 8) {
+            Toast.makeText(this, "Le mot de passe est trop court", Toast.LENGTH_LONG).show()
+            binding.password.requestFocus()
+            return false
+        }
+        return true
+    }
 
 
     }
+
+
+
+
+
+
+
+
+
+
 
 
